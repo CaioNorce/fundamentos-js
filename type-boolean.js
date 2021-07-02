df403c3e7d1fb5862d56a37de5edb7d44113bf96
Caio Norce <caionorce@gmail.com>
2021-07-01 21:03:27 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,12 @@
+// tipo booleano
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = false;
+
+//console.log(primeiroNumero === segundoNumero);
+
+
+const texto1 = "meu pastel é mais barato";
+const texto2 = "não é não!";
+console.log(texto1 == texto2);
---
+++ type-string.js
@@ -0,0 +1,22 @@
+const texto = "Olá, mundo!";
+console.log(texto);
+
+const texto2 = 'Olá, mundo!';
+console.log(texto2);
+
+const senha = "senhaSuperSegura456!"
+console.log(senha);
+
+const stringDeNumeros = "34567";
+console.log(stringDeNumeros);
+
+const citacao = "Meu nome é ";
+const meuNome = "Caio";
+
+//concatenação (+)
+
+console.log(citacao + meuNome);
+
+
+
+//template string ou template literal (pesquisar)
