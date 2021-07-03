b7137792f454a69ced5816ae860a1c4e2280e109
Caio Norce <caionorce@gmail.com>
2021-07-02 22:34:38 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,16 @@
+// tipo de dado
+// booleanos
+
+// conversão implícita
+
+const numero = 456;
+const numeroString = Number("456");
+console.log(numero + numeroString);
+
+// == compara apenas o valor interno e === compara o tipo 
+
+
+// conversão explícita
+
+//Number()
+//String()
